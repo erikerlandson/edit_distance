ce9b712cff2d9a8effe8e50be45adeb8cf8274ec
Erik Erlandson <eerlands@redhat.com>
2013-06-06 17:14:19 -0700

    edit_distance() stub

--- include/edit_distance.h
+++ include/edit_distance.h
@@ -14,6 +14,37 @@ http://www.boost.org/LICENSE_1_0.txt
 #if !defined(__edit_distance_h__)
 #define __edit_distance_h__ 1
 
+#include <boost/range/functions.hpp>
+#include <boost/range/metafunctions.hpp>
+
+template <typename Range>
+struct default_cost {
+    typedef typename boost::range_difference<Range>::type cost_type;
+    typedef typename boost::range_value<Range>::type value_type;
+    cost_type cost_ins(value_type const& a, value_type const& b) const {
+        return cost_type(1);
+    }
+    cost_type cost_del(value_type const& a, value_type const& b) const {
+        return cost_type(1);
+    }
+    cost_type cost_sub(value_type const& a, value_type const& b) const {
+        return (a == b) ? cost_type(0) : cost_type(1);
+    }
+};
+
+
+template <typename ForwardRange1, typename ForwardRange2, typename Cost>
+unsigned int edit_distance(ForwardRange1 const& seq1, ForwardRange2 const& seq2, Cost& cost) {
+    return 0;
+}
+
+template <typename ForwardRange1, typename ForwardRange2>
+inline
+unsigned int edit_distance(ForwardRange1 const& seq1, ForwardRange2 const& seq2) {
+    default_cost<ForwardRange1> cost;
+    return edit_distance(seq1, seq1, cost);
+}
+
 
 
 #endif
--- test/edit_distance.cpp
+++ test/edit_distance.cpp
@@ -1,10 +1,16 @@
 #include <boost/test/unit_test.hpp>
 
 #include "edit_distance.h"
+#include <vector>
 
-BOOST_AUTO_TEST_SUITE(edit_distance)
+using std::vector;
 
-BOOST_AUTO_TEST_CASE(skeleton_test) {
+BOOST_AUTO_TEST_SUITE(edit_distance_suite)
+
+BOOST_AUTO_TEST_CASE(empty_sequence) {
+    vector<int> t;
+    unsigned long d = edit_distance(t, t);
+    BOOST_CHECK_EQUAL(d, 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
