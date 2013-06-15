a9fb11465ea5a544f92010a925f48a4dd0f268e7
Erik Erlandson <eerlands@redhat.com>
2013-06-15 13:59:30 -0700

    function adaptor: composable, with basis case, and now with delayed semantic resolution via accumulated parameter list

--- test/test.cpp
+++ test/test.cpp
@@ -23,9 +23,17 @@ http://www.boost.org/LICENSE_1_0.txt
 #include <boost/mpl/next_prior.hpp>
 #include <boost/mpl/plus.hpp>
 #include <boost/mpl/arithmetic.hpp>
+#include <boost/mpl/push_back.hpp>
+#include <boost/mpl/fold.hpp>
 
 using boost::mpl::int_;
 using boost::mpl::insert;
+
+
+template <typename MPL_Seq> 
+struct mpl_seq_sum {
+    typedef typename boost::mpl::fold<MPL_Seq, int_<0>, boost::mpl::plus<boost::mpl::_1, boost::mpl::_2> >::type type;
+};
 
 template <typename Set, typename Arg>
 struct union_with {
@@ -40,21 +48,34 @@ int f() {
     return v;
 }
 
-template <typename F, typename Param>
-struct f_adaptor_type {
-    int operator()() {
-        return Param::value + ff();
-    }
-    F ff;
-};
+template <typename ParamList>
+int f_adaptor_impl(int v) {
+    return v + mpl_seq_sum<ParamList>::type::value;
+}
 
 template <typename Param>
 struct f_adaptor_type_basis {
+    // param list basis case:
+    typedef boost::mpl::vector<Param> param_list;
+    // note, if this adaptor is composed with another, this operator is ignored, only the accumulated param_list matters
     int operator()() {
         int v;
         f_impl(v);
-        return Param::value + v;
+        return f_adaptor_impl<param_list>(v);
     }
+};
+
+template <typename F, typename Param>
+struct f_adaptor_type {
+    // accumulating param_list from below:
+    typedef typename boost::mpl::push_back<typename F::param_list, Param>::type param_list;
+    // note, if this adaptor is composed with another, this operator is ignored, only the accumulated param_list matters
+    int operator()() {
+        int v;
+        f_impl(v);
+        return f_adaptor_impl<param_list>(v);
+    }
+    F ff;
 };
 
 template <typename Param, typename F>
@@ -79,4 +100,7 @@ BOOST_AUTO_TEST_CASE(adaptor) {
         //std::cout << boost::mpl::plus<n, boost::mpl::int_<1> >::value << "\n";
     std::cout << f_adaptor<int_<7> >(f)() << "\n";
     std::cout << f_adaptor<int_<7> >(f_adaptor<int_<7> >(f))() << "\n";
+    std::cout << boost::mpl::plus<int_<1>,int_<2> >::value << "\n";
+    typedef typename mpl_seq_sum<boost::mpl::vector<int_<1>,int_<2>,int_<3> > >::type sum_t; 
+    std::cout << sum_t::value << "\n";
 }
