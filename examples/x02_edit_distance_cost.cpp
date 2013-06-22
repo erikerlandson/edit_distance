f843b10f478decd7871b45f40e16867311014cf6
Erik Erlandson <eerlands@redhat.com>
2013-06-22 16:36:19 -0700

    added example comments

--- examples/x02_edit_distance_cost.cpp
+++ examples/x02_edit_distance_cost.cpp
@@ -20,8 +20,10 @@ using boost::algorithm::sequence_alignment::edit_distance;
 // define a custom cost function where insertion or deletion of space costs nothing
 struct cost_free_space {
     // note, these types are required for a cost function:
-    typedef unsigned cost_type;    // data type used to store costs
-    typedef char value_type;       // sequence elements must be assignable to this
+    typedef unsigned cost_type;  // edit_distance uses cost_type to store cost values internally
+                                 // you can define it smaller to save space, or define it as
+                                 // floating point to support non-integer costs, etc.
+    typedef char value_type;     // sequence elements must be assignable to value_type
 
     // inserting or deleting a space is free:
     cost_type cost_ins(value_type c) { return (c == ' ') ? 0 : 1; }
