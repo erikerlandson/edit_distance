1a1c64b8733c236d05073dbc4617258cfdf2caa2
Erik Erlandson <eerlands@redhat.com>
2013-10-23 17:44:01 -0700

    a draft of dijkstra_sssp_cost()

--- include/boost/algorithm/sequence_alignment/detail/edit_distance.hpp
+++ include/boost/algorithm/sequence_alignment/detail/edit_distance.hpp
@@ -20,6 +20,7 @@ http://www.boost.org/LICENSE_1_0.txt
 #include <boost/pool/object_pool.hpp>
 
 #include <boost/heap/priority_queue.hpp>
+#include <boost/heap/fibonacci_heap.hpp>
 
 #include <boost/range/metafunctions.hpp>
 
@@ -38,6 +39,17 @@ using boost::end;
 using boost::range_iterator;
 using boost::range_value;
 using boost::range_reference;
+
+using boost::algorithm::sequence_alignment::detail::path_head;
+using boost::algorithm::sequence_alignment::detail::path_lessthan;
+
+#if 0
+using boost::algorithm::sequence_alignment::detail::edit_opcode;
+using boost::algorithm::sequence_alignment::detail::ins_op;
+using boost::algorithm::sequence_alignment::detail::del_op;
+using boost::algorithm::sequence_alignment::detail::sub_op;
+using boost::algorithm::sequence_alignment::detail::eql_op;
+#endif
 
 template <typename ForwardRange1, typename ForwardRange2, typename Cost>
 typename Cost::cost_type 
@@ -75,6 +87,48 @@ needleman_wunsch_distance(ForwardRange1 const& seq1, ForwardRange2 const& seq2, 
         }
     }
     return *c01;
+}
+
+template <typename ForwardRange1, typename ForwardRange2, typename Cost>
+typename Cost::cost_type 
+dijkstra_sssp_cost(ForwardRange1 const& seq1, ForwardRange2 const& seq2, Cost& cost) {
+    typedef typename Cost::cost_type cost_t;
+    typedef typename range_iterator<ForwardRange1 const>::type itr1_t;
+    typedef typename range_iterator<ForwardRange2 const>::type itr2_t;
+    typedef path_head<itr1_t, itr2_t, cost_t> head_t;
+
+    const itr1_t beg1 = begin(seq1);
+    const itr1_t end1 = end(seq1);
+    itr1_t tt1 = end1; ++tt1;
+    const itr1_t z1 = tt1;
+    const itr2_t beg2 = begin(seq2);
+    const itr2_t end2 = end(seq2);
+    itr2_t tt2 = end2; ++tt2;
+    const itr2_t z2 = tt2;
+
+    boost::object_pool<head_t> pool;
+    boost::heap::fibonacci_heap<head_t*, boost::heap::compare<path_lessthan> > heap;
+
+    heap.push(pool.construct(z1, z2, cost_t(0)));
+    while (true) {
+        head_t* h = heap.top();
+        heap.pop();
+        if (h->j1 == end1) {
+            if (h->j2 == end2) break;
+            itr2_t j2 = (h->j2 == z2) ? beg2 : (++(h->j2));
+            heap.push(pool.construct(h->j1, j2, h->cost + cost.cost_ins(*j2)));
+        }  else {
+            itr1_t j1 = h->j1;
+            j1 = (j1 == z1) ? beg1 : (++j1);
+            heap.push(pool.construct(j1, h->j2, h->cost + cost.cost_del(*j1)));
+            if (h->j2 == end2) continue;
+            itr2_t j2 = (h->j2 == z2) ? beg2 : (++(h->j2));
+            heap.push(pool.construct(h->j1, j2, h->cost + cost.cost_ins(*j2)));     
+            heap.push(pool.construct(j1, j2, h->cost + cost.cost_sub(*j1, *j2)));            
+        }
+    }
+
+    return heap.top()->cost;
 }
 
 }}}}
--- include/boost/algorithm/sequence_alignment/detail/types.hpp
+++ include/boost/algorithm/sequence_alignment/detail/types.hpp
@@ -13,25 +13,28 @@ http://www.boost.org/LICENSE_1_0.txt
 #if !defined(BOOST_ALGORITHM_SEQUENCE_ALIGNMENT_DETAIL_TYPES_HPP)
 #define BOOST_ALGORITHM_SEQUENCE_ALIGNMENT_DETAIL_TYPES_HPP
 
-#include <boost/algorithm/sequence_alignment/edit_types.hpp>
-
 namespace boost {
 namespace algorithm {
 namespace sequence_alignment {
 namespace detail {
 
-using boost::algorithm::sequence_alignment::edit_opcode;
-
 template <typename Itr1, typename Itr2, typename Cost>
 struct path_head {
-    edit_opcode op;
     Itr1 j1;
     Itr2 j2;
     Cost cost;
 
     path_head() {}
     ~path_head() {}
-    path_head(const path_head& src) : op(src.op), j1(src.j1), j2(src.j2), cost(src.cost) {}
+    path_head(const path_head& src) : j1(src.j1), j2(src.j2), cost(src.cost) {}
+    path_head(const Itr1& j1_, const Itr2& j2_, const Cost& cost_) : j1(j1_), j2(j2_), cost(cost_) {}
+};
+
+
+struct path_lessthan {
+    template <typename T> bool operator()(T const* a, T const* b) const {
+        return a->cost > b->cost;
+    }
 };
 
 }}}}
