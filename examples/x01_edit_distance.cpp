e26fd57d37a98f0b4b5a1df23c1458ddf595d1af
Erik Erlandson <eerlands@redhat.com>
2013-10-31 07:46:45 -0700

    clean example of CREATE_MEMBER_CHECK()

--- examples/x01_edit_distance.cpp
+++ examples/x01_edit_distance.cpp
@@ -1,5 +1,5 @@
 /*******
-edit_distance: STL and Boost compatible edit distance functions for C++
+Demonstrate a Boost/MPL style macro to create predicate templates for class member checking
 
 Copyright (c) 2013 Erik Erlandson
 
@@ -18,33 +18,31 @@ http://www.boost.org/LICENSE_1_0.txt
 #include <boost/typeof/std/utility.hpp>
 
 
-// put working definitions inside a namespace \
-// when T and U both have a member 'member', this will induce an ambiguity failure: \
-// struct 'test' uses SFINAE to resolve to 'true' when T defines 'member' \
-// this specialization of 'test' matches when T does not define 'member' \
-// 'seed' exists to define a 'member', that will induce an ambiguity error when  \
+// This macro defines a predicate template to check a class for a member 'member'
+// 'member' can be any definition: member variable, member function, a nested type, etc
+// 
+// It works like this: first, it declares a namespace to cache working typedefs.
+// When T and U both have a member 'member', struct 'ambig' will induce an ambiguity failure
+// The struct 'test' uses SFINAE to resolve to 'true' when T defines 'member' \
+// The specialization of 'test' matches when T does *not* define 'member' \
+// Struct 'seed' exists to define a 'member', that will induce an ambiguity error when \
 // the class of interest also defines 'member' \
-
-// this macro defines a predicate template to check a class for a member 'member'
+// 
 #define CREATE_MEMBER_CHECK(member) \
 namespace __has_member_##member { \
- \
 template <typename T, typename U> struct ambig : public T, public U {}; \
- \
+\
 template <typename T, typename U, typename Enable = void> \
-struct test { \
-    static const bool value = true; \
-}; \
+struct test : public boost::true_type {}; \
 template <typename T, typename U> \
-struct test  <T, U, typename boost::enable_if<boost::is_same<BOOST_TYPEOF(&T:: member), BOOST_TYPEOF(&U:: member)> >::type> { \
-    static const bool value = false; \
-}; \
- \
+struct test<T, U, typename boost::enable_if<boost::is_same<BOOST_TYPEOF(&T:: member), BOOST_TYPEOF(&U:: member)> >::type> : public boost::false_type {}; \
+\
 struct seed { char member ; }; \
 } \
-template <typename T> struct has_member_##member : public \
-boost::integral_constant<bool, __has_member_##member::test<__has_member_##member::ambig<T, __has_member_##member::seed>, __has_member_##member::seed>::value> {}
+template <typename T> struct has_member_##member : public __has_member_##member::test<__has_member_##member::ambig<T, __has_member_##member::seed>, __has_member_##member::seed> {}
 
+
+// define a couple test classes
 // this test class has no members
 struct sss0 {};
 
@@ -52,12 +50,17 @@ struct sss0 {};
 struct sss1 { 
     void foo() {}
     int bar;
+    typedef int baz;
 };
 
-// define member-checking templates for 'foo' and 'bar'
+// define member-checking templates for 'foo', 'bar' and 'baz:
 CREATE_MEMBER_CHECK(foo);
 CREATE_MEMBER_CHECK(bar);
+CREATE_MEMBER_CHECK(baz);
 
+// Now demonstrate the checking templates in action.  Note that the template
+// inherits from either boost::true_type or boost::false_type, so it can be used in
+// all the same kind of MPL contexts 
 int main(int argc, char** argv) {
     // outputs 0 (false)
     std::cout << has_member_foo<sss0>::value << "\n";
@@ -68,6 +71,11 @@ int main(int argc, char** argv) {
     std::cout << has_member_bar<sss0>::value << "\n";
     // outputs 1 (true)
     std::cout << has_member_bar<sss1>::value << "\n";
+
+    // outputs 0 (false)
+    std::cout << has_member_baz<sss0>::value << "\n";
+    // outputs 1 (true)
+    std::cout << has_member_baz<sss1>::value << "\n";
 
     return 0;
 }
