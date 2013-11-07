2ac757df12204145603ac9819e83b05afdb13a4f
Erik Erlandson <eerlands@redhat.com>
2013-11-07 07:50:17 -0700

    A cross-checking unit test between edit_distance() and edit_alignment()

--- test/edit_alignment.cpp
+++ test/edit_alignment.cpp
@@ -186,5 +186,44 @@ BOOST_AUTO_TEST_CASE(timing_1) {
     BOOST_TEST_MESSAGE("time= " << tt << " sec   n= " << n << "   mean-time= " << tt/double(n) << "\n" );
 }
 
+BOOST_AUTO_TEST_CASE(crosscheck_1) {
+    char data[] = "abcdefghij0123456789";
+    const unsigned int data_size = sizeof(data)-1;
+    srand(73);
+    const unsigned int N = 1000;
+    const unsigned int LEN = 100;
+    const unsigned int D = 2;
+    const unsigned int R = LEN/D;
+    const unsigned int K = 25;
+    std::vector<std::string> seqdata(1000);
+    for (int i = 0;  i < seqdata.size();  ++i) {
+        seqdata[i].resize(LEN, 'x');
+        for (int d = 0;  d < D;  ++d) {
+            unsigned int b1 = d*R + (rand() % K);
+            unsigned int l1 =  rand() % K;
+            for (unsigned int j = b1;  j < b1+l1;  ++j) seqdata[i][j] = data[rand()%data_size];
+        }
+    }
+    int n = 0;
+    double t0 = time(0);
+    for (int i = 0;  i < seqdata.size();  ++i) {
+        if (n > N) break;
+        for (int j = 0;  j < i;  ++j) {
+            if (++n > N) break;
+            output_check_script_long_string out(seqdata[i], seqdata[j]);
+            unsigned int d1 = edit_alignment(seqdata[i], seqdata[j], _output = out, _cost = cost_mixed_ops());
+            unsigned int d2 = edit_distance(seqdata[i], seqdata[j], _cost = cost_mixed_ops());
+            out.finalize();
+            // verify that the edit script is a correct one: it transforms seq1 into seq2
+            BOOST_CHECK_MESSAGE(out.correct, "\n\nseq1= '" << seqdata[i] << "'\nseq2= '"<< seqdata[j] <<"'\n\n");
+            // cross-check that edit_alignment() and edit_distance() compute the same distance
+            BOOST_CHECK_MESSAGE(d1==d2, "\n\nseq1= '" << seqdata[i] << "'\nseq2= '"<< seqdata[j] <<"'\n\n");
+        }
+    }
+    n -= 1;
+    double tt = time(0) - t0;
+    BOOST_TEST_MESSAGE("time= " << tt << " sec   n= " << n << "   mean-time= " << tt/double(n) << "\n" );
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
