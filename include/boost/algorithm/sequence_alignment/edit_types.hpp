2bc31f1550a5d75021fc5c8da6d1765ca8254b1d
Erik Erlandson <eerlands@redhat.com>
2013-11-09 15:11:22 -0700

    remove nosub_unit_cost

--- include/boost/algorithm/sequence_alignment/edit_types.hpp
+++ include/boost/algorithm/sequence_alignment/edit_types.hpp
@@ -44,24 +44,6 @@ struct unit_cost {
     }
 };
 
-struct nosub_unit_cost {
-    typedef size_t cost_type;
-
-    template <typename value_type> inline
-    cost_type cost_ins(value_type const& a) const {
-        return cost_type(1);
-    }
-
-    template <typename value_type> inline
-    cost_type cost_del(value_type const& a) const {
-        return cost_type(1);
-    }
-
-    template <typename value_type_1, typename value_type_2> inline
-    cost_type cost_sub(value_type_1 const& a, value_type_2 const& b) const {
-        return (a == b) ? cost_type(0) : cost_type(10);
-    }
-};
 
 typedef unit_cost default_cost;
 
